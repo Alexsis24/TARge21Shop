d435b86ebc6567fa41135191b3fbf17da497ee95
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-01-19 09:34:01 +0200

    remove image

--- TARge21Shop/Controllers/SpaceshipsController.cs
+++ TARge21Shop/Controllers/SpaceshipsController.cs
@@ -11,15 +11,18 @@ namespace TARge21Shop.Controllers
     {
         private readonly TARge21ShopContext _context;
         private readonly ISpaceshipsServices _spaceshipsServices;
+        private readonly IFilesServices _filesServices;
 
         public SpaceshipsController
             (
                 TARge21ShopContext context,
-                ISpaceshipsServices spaceshipsServices
+                ISpaceshipsServices spaceshipsServices,
+                IFilesServices filesServices
             )
         {
             _context = context;
             _spaceshipsServices = spaceshipsServices;
+            _filesServices = filesServices;
         }
 
 
@@ -253,6 +256,23 @@ namespace TARge21Shop.Controllers
             var spaceshipId = await _spaceshipsServices.Delete(id);
 
             if (spaceshipId == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveImage(ImageViewModel file)
+        {
+            var dto = new FileToDatabaseDto()
+            {
+                Id = file.ImageId
+            };
+            var image = await _filesServices.RemoveImage(dto);
+
+            if (image == null)
             {
                 return RedirectToAction(nameof(Index));
             }
